3fcc2572a571ddaf845ad1ba7c794e886aea3e69
壹零 <guchengxi1994@qq.com>
2024-12-26 15:28:36 +0800

    update readme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:circular_menu/circular_menu.dart';
+import 'package:another_circular_menu/another_circular_menu.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -40,7 +40,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late final BoardController<MyItem> _controller = BoardController(items: [
     MyItem(index: 0, onItemSelected: (_) {}),
-    MyItem(index: 1, onItemSelected: (_) {})
+    MyItem(index: 1, onItemSelected: (_) {}),
+    MyItem(index: 2, onItemSelected: (_) {}),
+    MyItem(index: 3, onItemSelected: (_) {}),
+    MyItem(index: 4, onItemSelected: (_) {}),
+    MyItem(index: 5, onItemSelected: (_) {}),
+    MyItem(index: 6, onItemSelected: (_) {}),
   ]);
 
   @override
